b12ee70eb8f32fae606ba8b858f6f49fe06c5fc4
Rosalie1723 <Rosalie1723@outlook.com>
2026-05-07 16:15:50 +0800

    为collectExpandableIds添加注释

--- src/utils/collectExpandableIds.js
+++ src/utils/collectExpandableIds.js
@@ -1,3 +1,8 @@
+/**
+ * @param {Array<CatalogNode>} nodes - 搜索的根节点
+ * @param {string[]} collector - 末尾接收搜索结果
+ * @returns {string[]} 非叶节点的id数组
+ */
 export function collectExpandableIds(nodes, collector = []) {
   nodes.forEach((node) => {
     if (Array.isArray(node.children) && node.id) {
